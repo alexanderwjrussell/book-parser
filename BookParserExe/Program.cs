e86726a7be0a72d0d4adcc67002c0d546c7a575d
alexanderwjrussell <awjrussell@live.co.uk>
2017-01-18 20:38:07 +0000

    Console Application - Putting it all together 1) Created new console application project 2) Used the parser and prime checker 3) Output the results in a console window

---
+++ BookParserExe/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BookParserExe
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			var parser = new Parser.Parser();
+
+			var primeChecker = new Parser.PrimeChecker();
+
+			var countedWords = parser.CountWords(parser.Read("TheRailwayChildren.txt"));
+
+			foreach (var word in countedWords)
+			{
+				Console.WriteLine(word.Key + " : " + word.Value + " : " + primeChecker.IsPrime(word.Value));
+			}
+		}
+	}
+}
