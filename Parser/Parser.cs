ab6e5bf3c8b13b20f1d7ba95e35e21d1541da116
alexanderwjrussell <awjrussell@live.co.uk>
2017-01-18 10:18:54 +0000

    Project setup stage complete

---
+++ Parser/Parser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Parser
+{
+	public class Parser
+	{
+		public Parser()
+		{
+		}
+	}
+}
---
+++ ParserTests/ParserTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace ParserTests
+{
+	[TestFixture]
+	public class ParserTests
+	{
+		[Test]
+		public void TestCase()
+		{
+		}
+	}
+}
