07a54ec02a191e612d026564483ac0b39d4113a1
alexanderwjrussell <awjrussell@live.co.uk>
2017-01-18 19:08:08 +0000

    Test - Reading from file Part 2 1) Failing - Still only returning "not null" string

--- ParserTests/ParserTests.cs
+++ ParserTests/ParserTests.cs
@@ -7,11 +7,19 @@ namespace ParserTests
 	public class ParserTests
 	{
 		[Test]
-		public void Should_ReturnContentsFromBook_WhenAccessedFromFile()
+		public void Should_ReturnSomethingFromBook()
 		{
 			var parser = new Parser.Parser();
 
 			Assert.That(parser.Read("Cat.txt"), Is.Not.Null);
+		}
+
+		[Test]
+		public void Should_ReturnContentFromBook_WhenAccessedFromFile()
+		{
+			var parser = new Parser.Parser();
+
+			Assert.That(parser.Read("Cat.txt"), Is.EqualTo("The Cat in the Hat"));
 		}
 	}
 }
