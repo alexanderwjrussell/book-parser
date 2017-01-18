f297bd1cf152716dabc9863dd2798ab0f3844a1a
alexanderwjrussell <awjrussell@live.co.uk>
2017-01-18 19:42:36 +0000

    Test - Counting Words 1) Failing - CountWords method not yet implemented

--- Parser/Parser.cs
+++ Parser/Parser.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Parser
 {
 	public class Parser
@@ -6,6 +8,11 @@ namespace Parser
 		public string Read(string fileName)
 		{
 			return System.IO.File.ReadAllText(fileName);
+		}
+
+		public Dictionary<string, int> CountWords(string contents)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
--- ParserTests/ParserTests.cs
+++ ParserTests/ParserTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
 namespace ParserTests
 {
 	[TestFixture]
@@ -12,6 +14,23 @@ namespace ParserTests
 
 			Assert.That(parser.Read(fileName), Is.Not.Null);
 			Assert.That(parser.Read(fileName), Is.EqualTo(text));
+		}
+
+		[Test]
+		public void Should_CountEachTimeEachWordAppearsInABook()
+		{
+			var parser = new Parser.Parser();
+
+			var words = new Dictionary<string, int>
+			{
+				{"The", 1},
+				{"Cat", 1},
+				{"in", 1},
+				{"the", 1},
+				{"Hat", 1}
+			};
+
+			Assert.That(parser.CountWords("The Cat in the Hat"), Is.EqualTo(words));
 		}
 	}
 }
