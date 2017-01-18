bd79de596239eb23dd65fa0a52937317b8d8724c
alexanderwjrussell <awjrussell@live.co.uk>
2017-01-18 20:18:56 +0000

    Test - Prime Number Checker 1) Passing - Simple if statements and for loop to check for prime numbers

--- Parser/PrimeChecker.cs
+++ Parser/PrimeChecker.cs
@@ -4,6 +4,16 @@
 	{
 		public bool IsPrime(int number)
 		{
+			if (number == 1) return false;
+			if (number == 2) return true;
+
+			if (number % 2 == 0) return false; //Even number     
+
+			for (int i = 3; i < number; i += 2)
+			{
+				if (number % i == 0) return false;
+			}
+
 			return true;
 		}
 	}
